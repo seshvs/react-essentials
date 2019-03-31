14039c3f6cc64985c59e8c37fe98f4d847eeac9b
Seshadri VS <seshutou@gmail.com>
2019-03-31 14:19:40 +1100

    Add components page

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,38 +2,43 @@ import React, { Component } from 'react';
 import logo from '../assets/images/logo.svg';
 import '../assets/scss/App.scss';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Components from '../pages/Components';
 
 
 class App extends Component {
+
+  renderHeader = () => {
+    return (
+      <ul>
+          <li>
+            <Link className="active" to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/components">Components</Link>
+          </li>
+      </ul>
+    );
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <Router>
           <div>
-            <Header />
+            {this.renderHeader()}
 
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/topics" component={Topics} />
+            <Route path="/components" component={Components} />
           </div>
         </Router>
         </header>
-        <div className="App-body">
-         <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
 
-        </div>
       </div>
     );
   }
@@ -41,7 +46,26 @@ class App extends Component {
 
 
 function Home() {
-  return <h2>Home</h2>;
+  return (
+      <div>
+          <h2>Home</h2>
+          <div className="App-body">
+             <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+
+          </div>
+      </div>
+  );
 }
 
 function About() {
@@ -76,20 +100,5 @@ function Topics({ match }) {
   );
 }
 
-function Header() {
-  return (
-    <ul>
-      <li>
-        <Link className="active" to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/topics">Topics</Link>
-      </li>
-    </ul>
-  );
-}
 
 export default App;
---
+++ src/pages/Components.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+export default class Components extends Component {
+
+    render(){
+      return (
+          <div>
+            <h3> Component page. </h3>
+            <p> This page contain all the components which can be used to develop any website</p>
+          </div>
+      );
+    }
+};
